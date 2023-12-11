0f443cb7083c3282f1ea86fa513ca870ae5134e4
aniJani <rajkarnikarjanit@gmail.com>
2023-12-10 18:58:10 -0600

    Connect to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to Mongoose');
+})
+    .catch((err) => console.log(err));
 const app = express();
 
 app.listen(3000, (err, res) => {
