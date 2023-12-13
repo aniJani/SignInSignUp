829c94bb1f20663b32452b767f8a307cafc1aa6a
aniJani <rajkarnikarjanit@gmail.com>
2023-12-13 11:02:01 -0600

    add middleware and errorhandler

--- api/index.js
+++ api/index.js
@@ -19,3 +19,12 @@ app.listen(3000, (err, res) => {
 app.use(express.json());
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
