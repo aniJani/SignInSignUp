bb08e6b248d5b639f45c9547de43b0066cc98bf7
aniJani <rajkarnikarjanit@gmail.com>
2023-12-16 12:32:02 -0600

    sign in route created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,3 +14,21 @@ export const signup = async (req, res, next) => {
         next(err);
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email: email });
+        if (!validUser) return next(errorHandler(404, 'User not found'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials'))
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const expiryDate = new Date(Date.now() + 3600000);//1 hr
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+            .status(200)
+            .json(rest)
+    } catch (err) {
+        next(err);
+    }
+}
